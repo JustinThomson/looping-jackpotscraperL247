01693dd98b87f80b0b88ce1443519733c5299cbb
Justin Thomson <justin@gli-sa.co.za>
2021-06-08 01:02:44 +0200

    first commit

--- app.js
+++ app.js
@@ -1,103 +1,139 @@
 const express = require('express');
 const AWS = require('aws-sdk');
 const app = express();
+const puppeteer = require('puppeteer');
+const CronJob = require('cron').CronJob;
 require('dotenv').config();
 
-app.get('/', (req, res) => {
-    res.send("Nothing to see here");
-});
+// Choose the number of parallel requests to run (1 is best for this script purpose)
+const parallel = 1;
+ 
+const lotteries = 
+[
+    { name: 'Powerball', url: 'https://www.lotto247.com/en/play-lottery/powerball' },
+    { name: 'Powerball Plus', url: 'https://www.lotto247.com/en/play-lottery/powerball-plus' },
+    { name: 'Mega Millions', url: 'https://www.lotto247.com/en/play-lottery/megamillions' },
+    { name: 'Mega Millions Max', url: 'https://www.lotto247.com/en/play-lottery/mega-millions-max' }
+]
 
-app.listen(3000, () => {
+ const scrapelotteries = async (lotteries, parallel) => {
+   const parallelBatches = Math.ceil(lotteries.length / parallel)
+   const objectdata = [];
 
-    console.log('lets do this');
+   // Add new ad text into the adcopy variable below
+   let adcopy = 
+       {
+        "playnow": {
+            "en": "Play Now!",
+            "es": "Something spanish",
+            "pt": "something portuguese"
+            },
+        "somethingelse": {
+            "en": "Heres something cool!",
+            "es": "Something cool in spanish",
+            "pt": "something cool in portuguese"
+            } 
+        };
 
-    const puppeteer = require('puppeteer');
-    const CronJob = require('cron').CronJob;
-
-    const url = 'https://www.lotto247.com/en/play-lottery/powerball';
-
-    async function configureBrowser() {
-    const browser = await puppeteer.launch({
+   objectdata.push(adcopy);
+ 
+   console.log('\nI have the task of extracting data from ' + lotteries.length + ' pages and will visit ' + parallel + ' of them in paralell.');
+ 
+   console.log('This will result in ' + parallelBatches + ' batches.');
+   console.log('Lets do this!');
+ 
+   // Split up the Array of lotteries
+   let k = 0
+   for (let i = 0; i < lotteries.length; i += parallel) {
+     k++
+     console.log('\nBatch ' + k + ' of ' + parallelBatches)
+     // Launch and Setup Chromium
+     const browser = await puppeteer.launch({
         headless:true,
         args: ["--no-sandbox"]
     });
-    const page = await browser.newPage();
-    await page.setUserAgent('Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)');
-    await page.goto(url, {waitUntil: 'networkidle2'});
-    return page;
-}
-async function checkJackpot(page) {
+     // Fun with puppeteer
+     const context = await browser.createIncognitoBrowserContext()
+     const page = await context.newPage()
 
-    let html = await page.evaluate(() => document.body.innerHTML);
-    let lotteryname = await page.evaluate(() => document.querySelector('div.title.ng-star-inserted > h1').innerText);
-    let jackpot = await page.evaluate(() => document.querySelector('div.jackpot.ng-star-inserted').innerText);
-    let fullcountdown = await page.evaluate(() => document.querySelector('div.draw-time.ng-star-inserted > gli-game-counter > span').innerText);
-    let minutecountdown = fullcountdown.slice(0,-5);
-    let hourcountdown = fullcountdown.slice(0,-10);
-    let price = await page.evaluate(() => document.querySelector('div.ticket-price.ng-star-inserted').innerText);
-    let logo = await page.evaluate(() => document.querySelector('gli-lottery-game-banner > div > div > div > div > div > img').src);
-    
-    let pagedata = {
-        lotteryname,
-        jackpot,
-        fullcountdown,
-        minutecountdown,
-        hourcountdown,
-        price,
-        logo
-    }; 
+     const promises = []
+     for (let j = 0; j < parallel; j++) {
+       const elem = i + j
+       // only proceed if there is an element
+       if (lotteries[elem] !== undefined) {
+         // Promise to take Screenshots
+         // promises push
+         console.log('I promise to extract data from: ' + lotteries[elem].name)
+         promises.push(browser.newPage().then(async page => {
+           
+           try {
+             // Only create screenshot if page.goto get's no error
+             await page.setUserAgent('Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)');
+             await page.setViewport({ width: 1280, height: 800 })
+             await page.goto(lotteries[elem].url, {waitUntil: 'networkidle2'})
+             await page.waitForSelector('div.jackpot.ng-star-inserted', {visible: true,}); // wait for the jackpot amount  element to load
 
-    // convert JSON object to string
-    const writedata = JSON.stringify(pagedata);
+             let lotteryname = await page.evaluate(() => document.querySelector('div.title.ng-star-inserted > h1').innerText);
+             let jackpot = await page.evaluate(() => document.querySelector('div.jackpot.ng-star-inserted').innerText);
+             let fullcountdown = await page.evaluate(() => document.querySelector('div.draw-time.ng-star-inserted > gli-game-counter > span').innerText);
+             let minutecountdown = fullcountdown.slice(0,-5);
+             let hourcountdown = fullcountdown.slice(0,-10);
+             let price = await page.evaluate(() => document.querySelector('div.ticket-price.ng-star-inserted').innerText);
+             let logo = await page.evaluate(() => document.querySelector('gli-lottery-game-banner > div > div > div > div > div > img').src);
+             let pagedata = {
+                lotteryname,
+                jackpot,
+                fullcountdown,
+                minutecountdown,
+                hourcountdown,
+                price,
+                logo
+            };
+            
+            writedata = pagedata;
+            
+           } catch (err) {
+             console.log('Oops! I couldn\'t keep my promise to extract data from ' + lotteries[elem].name + '. Here is the error message:' + err)
+           }
+           objectdata.push(writedata);
+           console.log('\nThis data has been pushed into the writedata variable:\n' + JSON.stringify(writedata));
+         }))
+       }
+     }
+ 
+     // await promise all and close browser
+     await Promise.all(promises)
+     await browser.close()
+     console.log('\nI finished this batch. I\'m ready for the next batch')
+   }
 
-    //START OF NEW CODE FOR WRITING FILE TO S3 BUCKET
-    const fs = require('fs');
+   console.log('\nAll done with scraping the data and storing it in an object! Now lets write it to disk.\n')
 
-    // Enter copied or downloaded access id and secret here
-    const ID = process.env.aws_access_key_id;
-    const SECRET = process.env.aws_secret_access_key;
+  //write the file to disk
+  const fs = require('fs');
+  const filepath = './jackpotfeed.json';
+  fs.writeFile(filepath, JSON.stringify(objectdata), 'utf8', (err) => {
+      if (err) {
+          console.log('Error');
+      } else {
+          console.log ('\nI have saved the JSON file to\n' + filepath);
+          console.log("\nHere are the contents of the JSON:\n" + JSON.stringify(objectdata));
+      }
 
-    // Enter the name of the bucket that you have created here
-    const BUCKET_NAME = 'lottofeeds';
+  });
+  // End of writing to desk
 
-    // Initializing S3 Interface
-    const s3 = new AWS.S3({
-        accessKeyId: ID,
-        secretAccessKey: SECRET
-    });
 
-    const uploadFile = (fileName) => {
-        // read content from the file
-        const fileContent = writedata;
+ }
+ 
 
-        // setting up s3 upload parameters
-        const params = {
-            Bucket: BUCKET_NAME,
-            Key: 'powerball.json', // file name
-            Body: fileContent
-        };
 
-        // Uploading files to the bucket
-        s3.upload(params, function(err, data) {
-            if (err) {
-                throw err
-            }
-            console.log(`File uploaded successfully. ${data.Location}`)
-        });
-    };
 
-    uploadFile('powerball.json');
-    console.log(writedata);
-}
-
-async function startTracking() {
-    const page = await configureBrowser();
-    let job = new CronJob('*/5 * * * *', function(){ // runs every 5 minutes
-        checkJackpot(page);
+ async function startTracking(){
+    let job = new CronJob('*/60 * * * * *', function(){ // runs every 15 seconds
+        scrapelotteries(lotteries, parallel);
     }, null, true, null, null, true);
     job.start();
-}
+ }
 
-startTracking();
-
-});
+ startTracking();
